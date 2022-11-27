cbf4a924f7fa1128ad2cec4448d9830b67bc91f2
marieham45 <marie.hamsikova@centrum.cz>
2022-11-27 18:19:56 +0100

    Cistsi komponenta Task

--- Task/index.js
+++ Task/index.js
@@ -1,25 +1,16 @@
 export const Task = (props) => {
   const { name, due, done } = props;
-  let result = "";
+  let doneSign = "";
   if (done) {
-    result = `
-          <div class="task">
-              <div class="task__body">
-                <div class="task__name">${name}</div>
-                <div class="task__due">${due}</div>
-              </div>
-              <div class="task__done">✓</div>
-            </div>`;
-  } else {
-    result = `
-          <div class="task">
-              <div class="task__body">
-                <div class="task__name">${name}</div>
-                <div class="task__due">${due}</div>
-              </div>
-              <div class="task__done"></div>
-            </div>`;
+    doneSign = "✓";
   }
 
-  return result;
+  return `
+  <div class="task">
+      <div class="task__body">
+        <div class="task__name">${name}</div>
+        <div class="task__due">${due}</div>
+      </div>
+      <div class="task__done">${doneSign}</div>
+    </div>`;
 };
